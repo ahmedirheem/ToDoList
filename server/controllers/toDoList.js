48b050b4387fc32bd2a22cca9f7d70a41fb98a14
ahmedirheem <ahmedirheem2003@gmail.com>
2022-10-22 16:51:48 +0300

    Add route

--- server/controllers/toDoList.js
+++ server/controllers/toDoList.js
@@ -3,15 +3,13 @@ let home = (req, res)=>{
 }
 
 let add = (req, res)=>{
-    const {title,description,date,checked}=req.body;
+    const {title,description,date,checked} = req.body;
     let task = {
         title: title,
         description: description,
         date: date,
         checked:checked
     }
-
-
 }
 
 let completed = (req, res)=>{
